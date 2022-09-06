e5dea393757a58363dcb247a230a2eaf9b367508
Alexsixcent <alexsixcent@gmail.com>
2022-09-06 21:43:09 +0200

    Added no parameter overload to builder

--- ChatModApp.AuthCallback/Startup.cs
+++ ChatModApp.AuthCallback/Startup.cs
@@ -4,16 +4,16 @@ namespace ChatModApp.AuthCallback;
 
 public static class StartupHelpers
 {
-    public static WebApplicationBuilder CreateBuilder(WebApplicationOptions options)
+    public static WebApplicationBuilder CreateBuilder(WebApplicationOptions? options = null)
     {
-        var builder = WebApplication.CreateBuilder(options);
+        var builder = WebApplication.CreateBuilder(options ?? new());
 
         // Add services to the container.
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
         builder.Services.AddFluentUIComponents();
         builder.Services.AddHttpClient();
-
+        
         return builder;
     }
 
