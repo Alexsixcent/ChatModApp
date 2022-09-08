b2a9012ac6a9c24d4f6b9100f0e4f6c05516f6e6
Alexsixcent <alexsixcent@gmail.com>
2022-09-08 14:37:07 +0200

    Bootstrapper and Host start refactor

--- ChatModApp.Shared/Tools/Bootstrapper.cs
+++ ChatModApp.Shared/Tools/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reactive;
+using System.Reactive.Linq;
 using System.Reflection;
 using ChatModApp.Shared.Services;
 using ChatModApp.Shared.Services.ApiClients;
@@ -6,6 +7,7 @@ using ChatModApp.Shared.Tools.Extensions;
 using ChatModApp.Shared.ViewModels;
 using DryIoc;
 using DryIoc.Microsoft.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ReactiveUI;
@@ -19,43 +21,83 @@ using Splat.Serilog;
 
 namespace ChatModApp.Shared.Tools;
 
-public static class Bootstrapper
+public class Bootstrapper : IDisposable
 {
-    public static void Init(string logFolder, Action<IContainer>? additionalReg = null)
-    {
-        Log.Logger = new LoggerConfiguration()
-                     .MinimumLevel.Debug()
-                     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                     .MinimumLevel.Override("TwitchLib", LogEventLevel.Debug)
-                     .Enrich.FromLogContext()
-                     .WriteTo.Console()
-                     .WriteTo.Debug()
-                     .WriteTo.File(new CompactJsonFormatter(), Path.Combine(logFolder, "globalLogs.log"))
-                     .CreateLogger();
+    private readonly string _appName;
+    private readonly string _logFolder;
+    private IHostBuilder _hostBuilder;
+    private IHost? _activeHost;
 
-        Host.CreateDefaultBuilder()
+    public Bootstrapper(string applicationName, IHostBuilder? builder = null)
+    {
+        _appName = applicationName;
+        _hostBuilder = builder ?? Host.CreateDefaultBuilder();
+        _logFolder = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), applicationName);
+    }
+
+    public Bootstrapper Configure(Action<IContainer>? additionalReg = null)
+    {
+        Log.Logger = CreateLoggerConfig(_logFolder).CreateLogger();
+        
+        SetupSuspensionHost();
+
+        _hostBuilder = _hostBuilder
             .UseServiceProviderFactory(new DryIocServiceProviderFactory(new Container()))
             .ConfigureContainer<IContainer>(container =>
             {
                 additionalReg?.Invoke(container);
                 ConfigureServices(container);
             })
-            .ConfigureServices(services =>
-            {
-                services
-                    .AddRefitClient<IBttvApi>()
-                    .ConfigureHttpClient(c => c.BaseAddress = new("https://api.betterttv.net/3/cached"));
-
-                services
-                    .AddRefitClient<IFfzApi>()
-                    .ConfigureHttpClient(c => c.BaseAddress = new("https://api.frankerfacez.com/v1"));
-            })
+            .ConfigureServices(ConfigureHostServices)
             .UseSerilog()
-            .UseEnvironment(Environments.Development)
-            .Build();
-
+            .UseEnvironment(Environments.Development);
+        
         RxApp.DefaultExceptionHandler =
             Observer.Create<Exception>(ex => Log.Fatal(ex, "Unhandled exception occurred in observable"));
+
+        return this;
+    }
+
+    public async Task Start(string[] args, CancellationToken token = default)
+    {
+        _activeHost = await _hostBuilder.ConfigureHostConfiguration(builder => builder.AddCommandLine(args))
+            .StartAsync(token);
+    }
+
+    public async Task Stop(CancellationToken token = default)
+    {
+        if (_activeHost is null) return;
+        await _activeHost.StopAsync(token);
+
+        if (_activeHost is IAsyncDisposable disposable)
+            await disposable.DisposeAsync();
+        else
+            _activeHost.Dispose();
+    }
+
+    public void Dispose()
+    {
+        _activeHost?.Dispose();
+    }
+
+    private static void SetupSuspensionHost()
+    {
+        RxApp.SuspensionHost.CreateNewAppState = () => new AppState();
+        RxApp.SuspensionHost.SetupDefaultSuspendResume(new AkavacheSuspensionDriver<AppState>());
+        RxApp.SuspensionHost.WhenAnyValue(host => host.AppState)
+            .WhereNotNull()
+            .Cast<AppState>()
+            .Subscribe(state => Locator.CurrentMutable.RegisterConstant(state));
+    }
+
+    private static void ConfigureHostServices(IServiceCollection services)
+    {
+        services.AddRefitClient<IBttvApi>()
+            .ConfigureHttpClient(c => c.BaseAddress = new("https://api.betterttv.net/3/cached"));
+
+        services.AddRefitClient<IFfzApi>()
+            .ConfigureHttpClient(c => c.BaseAddress = new("https://api.frankerfacez.com/v1"));
     }
 
     private static void ConfigureServices(IContainer container)
@@ -88,4 +130,14 @@ public static class Bootstrapper
         container.Register<ChatTabItemViewModel>(setup: Setup.With(allowDisposableTransient: true));
         container.Register<ChatTabPromptViewModel>(setup: Setup.With(allowDisposableTransient: true));
     }
+
+    private static LoggerConfiguration CreateLoggerConfig(string folder, LoggerConfiguration? config = null) 
+        => (config ?? new LoggerConfiguration())
+        .MinimumLevel.Debug()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+        .MinimumLevel.Override("TwitchLib", LogEventLevel.Debug)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.Debug()
+        .WriteTo.File(new CompactJsonFormatter(), Path.Combine(folder, "globalLogs.log"));
 }
