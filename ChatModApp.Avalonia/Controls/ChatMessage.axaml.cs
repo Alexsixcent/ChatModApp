663c8fe83458bceac893c0d113dca8251c52890e
Alexsixcent <alexsixcent@gmail.com>
2023-01-07 19:21:51 -0800

    Better performance chat message display with Inlines, supports select

--- ChatModApp.Avalonia/Controls/ChatMessage.axaml.cs
+++ ChatModApp.Avalonia/Controls/ChatMessage.axaml.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Documents;
 using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
 using Avalonia.Data;
-using Avalonia.Layout;
 using Avalonia.Media;
 using ChatModApp.Shared.Models.Chat;
 using ChatModApp.Shared.Models.Chat.Emotes;
@@ -15,23 +15,22 @@ using FluentAvalonia.UI.Controls;
 
 namespace ChatModApp.Controls;
 
-//TODO: Rework with inline text and UI when it comes in 0.11
-[TemplatePart(ElementPanel, typeof(WrapPanel))]
-[TemplatePart(ElementBadgesPanel, typeof(StackPanel))]
-[TemplatePart(ElementUserBlock, typeof(TextBlock))]
+[TemplatePart(ElementMessageBlock, typeof(TextBlock))]
+[TemplatePart(ElementBadgeInlines, typeof(Span))]
+[TemplatePart(ElementFragInlines, typeof(Span))]
 public class ChatMessage : TemplatedControl
 {
     public static readonly StyledProperty<IEnumerable<IChatBadge>> BadgesProperty =
         AvaloniaProperty.Register<ChatMessage, IEnumerable<IChatBadge>>(nameof(Badges),
-                                                                        defaultBindingMode: BindingMode.OneTime);
+            defaultBindingMode: BindingMode.OneTime);
 
     public static readonly StyledProperty<IEnumerable<IMessageFragment>> MessageFragmentsProperty =
         AvaloniaProperty.Register<ChatMessage, IEnumerable<IMessageFragment>>(nameof(MessageFragments),
-                                                                              defaultBindingMode: BindingMode.OneTime);
+            defaultBindingMode: BindingMode.OneTime);
 
     public static readonly DirectProperty<ChatMessage, string> UsernameProperty =
         TextBlock.TextProperty.AddOwner<ChatMessage>(m => m.Username, (m, s) => m.Username = s, string.Empty,
-                                                     BindingMode.OneTime);
+            BindingMode.OneTime);
 
     public static readonly StyledProperty<Color> UsernameColorProperty =
         SolidColorBrush.ColorProperty.AddOwner<ChatMessage>();
@@ -60,89 +59,79 @@ public class ChatMessage : TemplatedControl
         set => SetValue(UsernameColorProperty, value);
     }
 
-    private const string ElementPanel = "PART_Panel",
-                         ElementBadgesPanel = "PART_BadgesPanel",
-                         ElementUserBlock = "PART_UserBlock";
+    private const string ElementMessageBlock = "PART_MessageBlock",
+        ElementBadgeInlines = "PART_BadgeInlines",
+        ElementFragInlines = "PART_FragInlines";
 
     private string _username = string.Empty;
 
-    private WrapPanel? _panel;
-    private StackPanel? _badgesPanel;
-    private TextBlock? _usernameBlock;
+    private TextBlock? _messageBlock;
+    private Span? _badgeInlines, _fragInlines;
 
     static ChatMessage()
     {
         BadgesProperty.Changed.AddClassHandler<ChatMessage>((message, args) => message.OnBadgesChanged(args));
         MessageFragmentsProperty.Changed.AddClassHandler<ChatMessage>((message, args) =>
-                                                                          message.OnFragmentsChanged(args));
+            message.OnFragmentsChanged(args));
     }
 
     private void OnBadgesChanged(AvaloniaPropertyChangedEventArgs args)
     {
-        if (args.NewValue is IEnumerable<IChatBadge> badges)
-        {
-            BuildBadges(badges);
-        }
+        if (args.NewValue is not IEnumerable<IChatBadge> badges) return;
+        
+        SetBadges(badges);
     }
 
     private void OnFragmentsChanged(AvaloniaPropertyChangedEventArgs args)
     {
-        if (args.NewValue is IEnumerable<IMessageFragment> frags)
-        {
-            RebuildFragments(frags);
-        }
+        if (args.NewValue is not IEnumerable<IMessageFragment> fragments) return;
+        
+        SetFragments(fragments);
     }
 
     protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
     {
-        _panel = e.NameScope.Find<WrapPanel>(ElementPanel);
-        _badgesPanel = e.NameScope.Find<StackPanel>(ElementBadgesPanel);
-        _usernameBlock = e.NameScope.Find<TextBlock>(ElementUserBlock);
-
-        BuildBadges(Badges);
-        RebuildFragments(MessageFragments);
+        _messageBlock = e.NameScope.Find<TextBlock>(ElementMessageBlock);
+        _badgeInlines = e.NameScope.Find<Span>(ElementBadgeInlines);
+        _fragInlines = e.NameScope.Find<Span>(ElementFragInlines);
 
         base.OnApplyTemplate(e);
+        
+        SetBadges(Badges);
+        SetFragments(MessageFragments);
     }
 
-    private void BuildBadges(IEnumerable<IChatBadge> badges)
+    private void SetBadges(IEnumerable<IChatBadge> badges)
     {
-        _badgesPanel?.Children.Clear();
-        _badgesPanel?.Children.AddRange(badges.Select(GetControlFromBadge));
+        _badgeInlines?.Inlines.Clear();
+        _badgeInlines?.Inlines.AddRange(badges.Select(GetControlFromBadge));        
     }
 
-    private void RebuildFragments(IEnumerable<IMessageFragment> fragments)
+    private void SetFragments(IEnumerable<IMessageFragment> fragments)
     {
-        if (_panel?.Children.Count > 2)
-        {
-            _panel?.Children.RemoveRange(2, _panel.Children.Count - 2);
-        }
-
-        _panel?.Children.AddRange(fragments.Select(GetMsgFragControl));
+        _fragInlines?.Inlines.Clear();
+        _fragInlines?.Inlines.AddRange(fragments.Select(GetMsgFragControl));
     }
 
-    private static Control GetControlFromBadge(IChatBadge badge) => new ChatBadge{Badge = badge};
+    private static Inline GetControlFromBadge(IChatBadge badge) =>
+        new InlineUIContainer(new ChatBadge {Badge = badge});
 
-    private static Control GetControlFromEmote(IEmote emote) => new ChatEmote { Emote = emote };
+    private static Inline GetControlFromEmote(IEmote emote) =>
+        new InlineUIContainer(new ChatEmote {Emote = emote});
 
-    private static Control GetMsgFragControl(IMessageFragment frag)
+    private static Inline GetMsgFragControl(IMessageFragment frag)
     {
         return frag switch
         {
             EmoteFragment emoteFragment => GetControlFromEmote(emoteFragment.Emote),
-            TextFragment textFragment => new TextBlock
-            {
-                Text = textFragment.Text,
-                TextWrapping = TextWrapping.Wrap,
-                TextAlignment = TextAlignment.Left,
-                VerticalAlignment = VerticalAlignment.Center
-            },
-            UriFragment uriFragment => new HyperlinkButton
-            {
-                Content = uriFragment.Text,
-                NavigateUri = uriFragment.Uri,
-                Padding = new(5, 5, 5, 6)
-            },
+            TextFragment textFragment => new Run(textFragment.Text),
+            UriFragment uriFragment => new InlineUIContainer(
+                new HyperlinkButton
+                {
+                    Content = uriFragment.Text,
+                    NavigateUri = uriFragment.Uri,
+                    Padding = new(5, 5, 5, 6)
+                }),
             _ => throw new ArgumentOutOfRangeException(nameof(frag))
         };
     }
